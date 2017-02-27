2c366903ec04ef9f8b7de3e67ef3d816e2086f33
cdavis19 <cjdavis6791@gmail.com>
2017-02-27 08:53:14 -0500

    init

---
+++ controller.js
@@ -0,0 +1,16 @@
+var app = angular.module('myMod');
+
+app.controller('reddit', function($http, $scope) {
+  $scope.redditData = {};
+
+  $http({
+    method: 'GET',
+    url: 'http://www.reddit.com/r/awww/.json'
+  }).then(function successfulCallback(response){
+    console.log(response);
+    $scope.redditData = response.data.data.children
+
+  });
+
+
+});
